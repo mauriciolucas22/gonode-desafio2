93c364f0939a96dd49e3d6b9172bfbf72e4cb178
Mauricio Lucas <mauricio.nq@hotmail.com>
2018-09-03 20:38:45 -0300

    nunjucks configurado

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const nunjucks = require('nunjucks');
+const path = require('path');
 
 const app = express();
-
-app.use(bodyParser.urlencoded({ extended: false }));
 
 const PORT = 3000;
 const HOST = '127.0.0.1';
 
+nunjucks.configure(path.resolve('app', 'views'), {
+  autoescape: true,
+  express: app,
+});
+
+app.set('view engine', 'html');
+app.use(express.static(path.resolve('app', 'views')));
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.get('/', (req, res) => {
-  res.send('Jesus loves you!');
+  res.render('signin');
 });
 
 app.listen(PORT, HOST);
