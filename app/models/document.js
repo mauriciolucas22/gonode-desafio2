af8f5f5c82aa6b34f9fa152da6d697ca14ac0924
Mauricio Lucas <mauricio.nq@hotmail.com>
2018-09-17 18:02:46 -0300

    renderiza markdown sem highlightjs

--- app/models/document.js
+++ app/models/document.js
@@ -1,7 +1,24 @@
+const md = require('markdown-it')();
+
 module.exports = (sequelize, DataTypes) => {
   const Document = sequelize.define('Document', {
     title: DataTypes.STRING,
     content: DataTypes.TEXT,
+  }, {
+    // cria novos campos pro model
+    getterMethods: {
+      excerpt() {
+        // this => se refere ao document que estiver acessando o excerpt
+        return this.title.length > 20
+          ? `${this.title.substring(0, 20)} ...`
+          : this.title;
+      },
+
+      // transforma o conteudo do markdown em HTML
+      formattedContent() {
+        return md.render(this.content);
+      },
+    },
   });
 
   Document.associate = (models) => {
